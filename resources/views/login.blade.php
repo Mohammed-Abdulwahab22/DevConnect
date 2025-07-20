ddf33518bce4423e176cb6796ff2f03f13de324e
Mohammed Abdulwahab <MohammedAbdulwahab22_@outlook.com>
2025-07-20 14:23:56 +0300

    modifying login page design.

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,9 +12,6 @@
     <div class="h-screen flex justify-center items-center backdrop-brightness-50">
         <div class="flex flex-col items-center space-y-8">
             <div>
-                <img src="https://res.cloudinary.com/dkt1t22qc/image/upload/v1742348949/Prestataires_Documents/smj7n1bdlpjsfsotwpco.png"
-                    alt="TyBot Logo" class="cursor-pointer" />
-            </div>
             <div class="rounded-[20px] w-80 p-8 bg-[#310D84]" style="box-shadow:-6px 3px 20px 4px #0000007d">
                 <h1 class="text-white text-3xl font-bold mb-4">Login</h1>
                 <div class="space-y-4">
