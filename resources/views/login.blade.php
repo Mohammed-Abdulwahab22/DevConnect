9410b1aba0c54069139646c8eca4e8b5d61d631f
Mohammed Abdulwahab <MohammedAbdulwahab22_@outlook.com>
2025-07-20 14:29:30 +0300

    creating and adding new logo in the login page.

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,7 +2,7 @@
 <link rel="preload" as="image"
     href="https://res.cloudinary.com/dkt1t22qc/image/upload/v1742348949/Prestataires_Documents/smj7n1bdlpjsfsotwpco.png" />
 {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+<script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 <header>
     <title>DevConnect</title>
 </header>
@@ -12,30 +12,42 @@
     <div class="h-screen flex justify-center items-center backdrop-brightness-50">
         <div class="flex flex-col items-center space-y-8">
             <div>
-            <div class="rounded-[20px] w-80 p-8 bg-[#310D84]" style="box-shadow:-6px 3px 20px 4px #0000007d">
-                <h1 class="text-white text-3xl font-bold mb-4">Login</h1>
-                <div class="space-y-4">
-                    <input type="text" placeholder="Email address"
-                        class="bg-[#8777BA] w-full p-2.5 rounded-md placeholder:text-gray-300 shadow-md shadow-blue-950" /><input
-                        type="text" placeholder="Password"
-                        class="bg-[#8777BA] w-full p-2.5 rounded-md placeholder:text-gray-300 shadow-md shadow-blue-950" />
+
+                <div class="flex items-center space-x-3 mb-8">
+                    <div class="relative w-12 h-12">
+                        <div class="absolute w-8 h-8 rounded-lg transform rotate-45 bg-gradient-to-br from-[#7336FF] to-[#3269FF]"
+                            style="box-shadow:-4px 2px 10px 2px rgba(0, 0, 0, 0.6);"></div>
+                        <div class="absolute w-8 h-8 rounded-lg transform -rotate-30 top-3 left-3 bg-[#310D84] border-2 border-[#7337FF]"
+                            style="box-shadow:-4px 2px 10px 2px rgba(0, 0, 0, 0.6);"></div>
+                    </div>
+                    <span class="text-4xl font-extrabold text-white">DevConnect</span>
                 </div>
-                <div class="mb-4">
-                    <span class="text-[#228CE0] text-[10px] ml-2 cursor-pointer">Forget Password?</span>
-                </div>
-                <div class="flex justify-center mb-4">
-                    <button
-                        class="h-10 w-full cursor-pointer text-white rounded-md bg-gradient-to-br from-[#7336FF] to-[#3269FF] shadow-md shadow-blue-950">
-                        Sign In
-                    </button>
-                </div>
-                <div class="text-gray-300 text-center">
-                    Don&#x27;t have an account?<span class="text-[#228CE0] cursor-pointer">
-                        Sign up</span>
+
+
+                <div class="rounded-[20px] w-80 p-8 bg-[#310D84]" style="box-shadow:-6px 3px 20px 4px #0000007d">
+                    <h1 class="text-white text-3xl font-bold mb-4">Login</h1>
+                    <div class="space-y-4">
+                        <input type="text" placeholder="Email address"
+                            class="bg-[#8777BA] w-full p-2.5 rounded-md placeholder:text-gray-300 shadow-md shadow-blue-950" /><input
+                            type="text" placeholder="Password"
+                            class="bg-[#8777BA] w-full p-2.5 rounded-md placeholder:text-gray-300 shadow-md shadow-blue-950" />
+                    </div>
+                    <div class="mb-4">
+                        <span class="text-[#228CE0] text-[10px] ml-2 cursor-pointer">Forget Password?</span>
+                    </div>
+                    <div class="flex justify-center mb-4">
+                        <button
+                            class="h-10 w-full cursor-pointer text-white rounded-md bg-gradient-to-br from-[#7336FF] to-[#3269FF] shadow-md shadow-blue-950">
+                            Sign In
+                        </button>
+                    </div>
+                    <div class="text-gray-300 text-center">
+                        Don&#x27;t have an account?<span class="text-[#228CE0] cursor-pointer">
+                            Sign up</span>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-</html>
+    </html>
