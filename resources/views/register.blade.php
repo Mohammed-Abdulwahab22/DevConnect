691047aad4f6291f73d5c7403b7be0bb34089574
Mohammed Abdulwahab <MohammedAbdulwahab22_@outlook.com>
2025-07-20 14:30:12 +0300

    adding the logo in the register page.

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,11 +9,23 @@
         href="https://res.cloudinary.com/dkt1t22qc/image/upload/v1742348949/Prestataires_Documents/smj7n1bdlpjsfsotwpco.png" />
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
+
 <body class="bg-cover bg-gradient-to-br from-[#7337FF] via-[#000000] to-[#0C7EA8]"
     style="background-image:url(https://res.cloudinary.com/dkt1t22qc/image/upload/v1742348950/Prestataires_Documents/fopt5esl9cgvlcawz1z4.jpg)">
     <div class="h-screen flex justify-center items-center backdrop-brightness-50">
         <div class="flex flex-col items-center space-y-8">
-        
+
+            <div class="flex items-center space-x-3 mb-8">
+                <div class="relative w-12 h-12">
+                    <div class="absolute w-8 h-8 rounded-lg transform rotate-45 bg-gradient-to-br from-[#7336FF] to-[#3269FF]"
+                        style="box-shadow:-4px 2px 10px 2px rgba(0, 0, 0, 0.6);"></div>
+                    <div class="absolute w-8 h-8 rounded-lg transform -rotate-30 top-3 left-3 bg-[#310D84] border-2 border-[#7337FF]"
+                        style="box-shadow:-4px 2px 10px 2px rgba(0, 0, 0, 0.6);"></div>
+                </div>
+                <span class="text-4xl font-extrabold text-white">DevConnect</span>
+            </div>
+
+
             <div class="rounded-[20px] w-80 p-8 bg-[#310D84]" style="box-shadow:-6px 3px 20px 4px #0000007d">
                 <h1 class="text-white text-3xl font-bold mb-4">Register</h1>
                 <div class="space-y-4">
@@ -45,4 +58,5 @@
         </div>
     </div>
 </body>
+
 </html>
