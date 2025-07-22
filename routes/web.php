2c55815b53dbf52412784d685116b4be38ec703b
Mohammed Abdulwahab <MohammedAbdulwahab22_@outlook.com>
2025-07-22 10:33:26 +0300

    making login page the root page.

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('login');
 });
 
 Route::get('/register', function () {
     return view('register');
 });
 
-Route::get('/login', function () {
-    return view('login');
+Route::get('/home', function () {
+    return view('home');
 });
 
 Route::get('/profile', function () {
