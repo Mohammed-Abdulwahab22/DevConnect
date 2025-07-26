2cd95f46abdcb266f9c4146e0020cc6271f1cfa4
Mohammed Abdulwahab <MohammedAbdulwahab22_@outlook.com>
2025-07-26 12:41:57 +0300

    fixing internal error.

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
+use App\Http\Controllers\ProfileSettingsController; 
 Route::get('/', [AuthController::class, 'showLoginForm'])->name('login');
 
 Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
