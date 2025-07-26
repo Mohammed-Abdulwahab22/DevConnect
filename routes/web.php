3903c1a160f4eef66a873e7840623547c04c256c
Mohammed Abdulwahab <MohammedAbdulwahab22_@outlook.com>
2025-07-26 12:35:57 +0300

    adding routes for the settings page.

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\AuthController;
 Route::get('/', [AuthController::class, 'showLoginForm'])->name('login');
 
 Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
-Route::post('/register', [AuthController::class, 'register']); 
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::post('/login', [AuthController::class, 'login']);
 
@@ -24,4 +24,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/settings', function () {
         return view('settings');
     })->name('settings');
+
+    Route::get('/settings', [ProfileSettingsController::class, 'edit'])->name('settings.edit');
+    Route::put('/profile-settings', [ProfileSettingsController::class, 'updateGeneral'])->name('profile.update');
+    Route::put('/profile-settings/profile-image', [ProfileSettingsController::class, 'updateProfileImage'])->name('profile.updateImage');
+    Route::put('/profile-settings/notifications', [ProfileSettingsController::class, 'updateNotifications'])->name('profile.updateNotifications');
+    Route::put('/profile-settings/password', [ProfileSettingsController::class, 'updatePassword'])->name('profile.updatePassword');
+    Route::delete('/profile-settings/delete-account', [ProfileSettingsController::class, 'deleteAccount'])->name('profile.deleteAccount');
 });
